d4dbeb6ebc1c8ba0fa1d982e841e6ca179503d21
Ehsan Ghasemi <ehsang1855@gmail.com>
2020-02-17 19:56:21 +0330

    add blog app

--- test/settings.py
+++ test/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'posts',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
