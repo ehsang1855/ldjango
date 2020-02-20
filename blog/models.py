7bd09f4ccf21d30d9a979f157293a077eaf047ba
Ehsan Ghasemi <ehsang1855@gmail.com>
2020-02-20 15:21:03 +0330

    Add create update delete post

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -9,5 +10,9 @@ class Post(models.Model):
         on_delete = models.CASCADE,
     )
     body = models.TextField()
+
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blog/', views.BlogListView.as_view(), name='blog'),
-    path('blog/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('post/', views.BlogListView.as_view(), name='blog'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.BlogDeleteView.as_view(), name='post_delete'),
+
 ]
